358b82a477babde516d2de809649f4821a051759
leoeco2000 <leoeco2000@qq.com>
2018-06-28 13:16:40 +0800

    JMH

--- src/test/java8Test/Java8InAction/chap7/ParallelStreamBenchmark.java
+++ src/test/java8Test/Java8InAction/chap7/ParallelStreamBenchmark.java
@@ -3,7 +3,6 @@ package test.java8Test.Java8InAction.chap7;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
-
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -19,44 +18,44 @@ import org.openjdk.jmh.annotations.Warmup;
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Fork(value=2, jvmArgs={"-Xms4G", "-Xmx4G"})
-@Measurement(iterations=2)
-@Warmup(iterations=3)
+@Fork(value = 2, jvmArgs = {"-Xms4G", "-Xmx4G"})
+@Measurement(iterations = 2)
+@Warmup(iterations = 3)
 public class ParallelStreamBenchmark {
 
-    private static final long N = 10_000_000L;
+  private static final long N = 10_000_000L;
 
-    @Benchmark
-    public long iterativeSum() {
-        long result = 0;
-        for (long i = 1L; i <= N; i++) {
-            result += i;
-        }
-        return result;
+  @Benchmark
+  public long iterativeSum() {
+    long result = 0;
+    for (long i = 1L; i <= N; i++) {
+      result += i;
     }
+    return result;
+  }
 
-    @Benchmark
-    public long sequentialSum() {
-        return Stream.iterate( 1L, i -> i + 1 ).limit(N).reduce( 0L, Long::sum );
-    }
+  @Benchmark
+  public long sequentialSum() {
+    return Stream.iterate(1L, i -> i + 1).limit(N).reduce(0L, Long::sum);
+  }
 
-    @Benchmark
-    public long parallelSum() {
-        return Stream.iterate(1L, i -> i + 1).limit(N).parallel().reduce( 0L, Long::sum);
-    }
+  @Benchmark
+  public long parallelSum() {
+    return Stream.iterate(1L, i -> i + 1).limit(N).parallel().reduce(0L, Long::sum);
+  }
 
-    @Benchmark
-    public long rangedSum() {
-        return LongStream.rangeClosed( 1, N ).reduce( 0L, Long::sum );
-    }
+  @Benchmark
+  public long rangedSum() {
+    return LongStream.rangeClosed(1, N).reduce(0L, Long::sum);
+  }
 
-    @Benchmark
-    public long parallelRangedSum() {
-        return LongStream.rangeClosed(1, N).parallel().reduce( 0L, Long::sum);
-    }
+  @Benchmark
+  public long parallelRangedSum() {
+    return LongStream.rangeClosed(1, N).parallel().reduce(0L, Long::sum);
+  }
 
-    @TearDown(Level.Invocation)
-    public void tearDown() {
-        System.gc();
-    }
+  @TearDown(Level.Invocation)
+  public void tearDown() {
+    System.gc();
+  }
 }
